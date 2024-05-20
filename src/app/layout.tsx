3193a21cfb6e03dc7bfca13b7ee92b7379373bd1
Yandy <riandyhsn@gmail.com>
2024-05-20 20:35:37 +0700

    fix: build problem

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,16 @@
-import '@/styles/globals.scss'
+import '@/styles/globals.scss';
 // Next.js allows you to import CSS directly in .js files.
 // It handles optimization and all the necessary Webpack configuration to make this work.
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-import ProgressBar from '@/components/ProgressBar/ProgressBar'
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import ProgressBar from '@/components/ProgressBar/ProgressBar';
+import { Suspense } from 'react';
 
 // You change this configuration value to false so that the Font Awesome core SVG library
 // will not try and insert <style> elements into the <head> of the page.
 // Next.js blocks this from happening anyway so you might as well not even try.
 // See https://fontawesome.com/v6/docs/web/use-with/react/use-with#next-js
-config.autoAddCss = false
+config.autoAddCss = false;
 
 export default function RootLayout({
   children,
@@ -17,11 +18,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>
-        <ProgressBar />
+        <Suspense>
+          <ProgressBar />
+        </Suspense>
         {children}
       </body>
     </html>
-  )
+  );
 }
